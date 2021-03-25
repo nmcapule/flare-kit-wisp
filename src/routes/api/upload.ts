1de48a25622ab46578abf1ee656b5cb574fd110f
Nathaniel M. Capule <nmcapule@gmail.com>
2021-03-25 10:31:02 +0000

    add /blog/*

--- src/routes/api/upload.ts
+++ src/routes/api/upload.ts
@@ -1,6 +1,6 @@
 import type { Request, SKResponse as Response } from '@sveltejs/kit/types.internal';
 
-export async function post(request: Request, context: any): Promise<Partial<Response>> {
+export async function get(request: Request, context: any): Promise<Partial<Response>> {
 	return {
 		body: {
 			hello: 'world'

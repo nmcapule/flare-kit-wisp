03837839dbd4b82d289bd10fa874c16b0402fe46
Nathaniel M. Capule <nmcapule@gmail.com>
2021-03-25 10:42:51 +0000

    mvoe [slug] to test/ and remove load/getStaticProps.

---
+++ src/routes/test/[slug].svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let message = 'This is the default value';
+</script>
+
+<pre>
+    <div>Message from $load: <code>{message}</code></div>
+    <div>$page from $app/stores: {JSON.stringify($page, null, 2)}</div>
+</pre>
